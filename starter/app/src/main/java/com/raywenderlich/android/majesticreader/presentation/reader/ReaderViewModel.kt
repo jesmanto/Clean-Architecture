44ed0d1d13e18049c051ba1139857065eaab7ef8
jesmanto <korojesse@gmail.com>
2022-03-02 16:39:10 +0100

    rendering PDFs classes

--- starter/app/src/main/java/com/raywenderlich/android/majesticreader/presentation/reader/ReaderViewModel.kt
+++ starter/app/src/main/java/com/raywenderlich/android/majesticreader/presentation/reader/ReaderViewModel.kt
@@ -43,95 +43,135 @@ import com.raywenderlich.android.majesticreader.domain.Bookmark
 import com.raywenderlich.android.majesticreader.domain.Document
 import com.raywenderlich.android.majesticreader.framework.Interactors
 import com.raywenderlich.android.majesticreader.framework.MajesticViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 class ReaderViewModel(application: Application, interactors: Interactors) : MajesticViewModel
-(application, interactors) {
+    (application, interactors) {
 
-  companion object {
-    private const val DOCUMENT_ARG = "document"
+    companion object {
+        private const val DOCUMENT_ARG = "document"
 
-    fun createArguments(document: Document) = bundleOf(
-        DOCUMENT_ARG to document
-    )
-  }
-
-  val document = MutableLiveData<Document>()
-
-  val bookmarks = MediatorLiveData<List<Bookmark>>().apply {
-    // TODO add sources
-  }
-
-  val currentPage = MediatorLiveData<PdfRenderer.Page>()
-
-  val hasPreviousPage: LiveData<Boolean> = Transformations.map(currentPage) {
-    it.index > 0
-  }
-
-  val hasNextPage: LiveData<Boolean> = Transformations.map(currentPage) {
-    renderer.value?.let { renderer -> it.index < renderer.pageCount - 1 }
-  }
-
-  val isBookmarked = MediatorLiveData<Boolean>().apply {
-    addSource(document) { value = isCurrentPageBookmarked() }
-    addSource(currentPage) { value = isCurrentPageBookmarked() }
-    addSource(bookmarks) { value = isCurrentPageBookmarked() }
-  }
-
-  val isInLibrary: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>().apply {
-    addSource(document) { value = isInLibrary(it) }
-  }
-
-  val renderer = MediatorLiveData<PdfRenderer>().apply {
-    addSource(document) {
-      try {
-        val pdfRenderer = getFileDescriptor(Uri.parse(it.url))?.let { it1 -> PdfRenderer(it1) }
-        value = pdfRenderer
-      } catch (e: IOException) {
-        e.printStackTrace()
-      }
-    }
-  }
-
-  private fun getFileDescriptor(uri: Uri) = application.contentResolver.openFileDescriptor(uri, "r")
-
-  private fun isCurrentPageBookmarked() =
-      bookmarks.value?.any { it.page == currentPage.value?.index } == true
-
-  // TODO check if document is in library
-  private fun isInLibrary(document: Document) = false
-
-  fun loadArguments(arguments: Bundle?) {
-    if (arguments == null) {
-      return
+        fun createArguments(document: Document) = bundleOf(
+            DOCUMENT_ARG to document
+        )
     }
 
-    // TODO load document from arguments and initialize
-  }
+    val document = MutableLiveData<Document>()
 
-  fun openDocument(uri: Uri) {
-    // TODO open document
-  }
+    val bookmarks = MediatorLiveData<List<Bookmark>>().apply {
+        addSource(document) { document ->
+            GlobalScope.launch {
+                postValue(interactors.getBookmarks(document))
+            }
+        }
+    }
 
-  fun openBookmark(bookmark: Bookmark) {
-    openPage(bookmark.page)
-  }
+    val currentPage = MediatorLiveData<PdfRenderer.Page>()
 
-  private fun openPage(page: Int) = renderer.value?.let {
-    currentPage.value = it.openPage(page)
-  }
+    val hasPreviousPage: LiveData<Boolean> = Transformations.map(currentPage) {
+        it.index > 0
+    }
 
-  fun nextPage() = currentPage.value?.let { openPage(it.index.plus(1)) }
+    val hasNextPage: LiveData<Boolean> = Transformations.map(currentPage) {
+        renderer.value?.let { renderer -> it.index < renderer.pageCount - 1 }
+    }
 
-  fun previousPage() = currentPage.value?.let { openPage(it.index.minus(1)) }
+    val isBookmarked = MediatorLiveData<Boolean>().apply {
+        addSource(document) { value = isCurrentPageBookmarked() }
+        addSource(currentPage) { value = isCurrentPageBookmarked() }
+        addSource(bookmarks) { value = isCurrentPageBookmarked() }
+    }
 
-  fun reopenPage() = openPage(currentPage.value?.index ?: 0)
+    val isInLibrary: MediatorLiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        addSource(document) { document -> GlobalScope.launch { postValue(isInLibrary(document)) } }
+    }
 
-  fun toggleBookmark() {
-    // TODO toggle bookmark on the current page
-  }
 
-  fun toggleInLibrary() {
-    // TODO toggle if open document is in library
-  }
+    val renderer = MediatorLiveData<PdfRenderer>().apply {
+        addSource(document) {
+            try {
+                val pdfRenderer =
+                    getFileDescriptor(Uri.parse(it.url))?.let { it1 -> PdfRenderer(it1) }
+                value = pdfRenderer
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getFileDescriptor(uri: Uri) =
+        application.contentResolver.openFileDescriptor(uri, "r")
+
+    private fun isCurrentPageBookmarked() =
+        bookmarks.value?.any { it.page == currentPage.value?.index } == true
+
+    // TODO check if document is in library
+    private suspend fun isInLibrary(document: Document) =
+        interactors.getDocuments().any { it.url == document.url }
+
+
+    fun loadArguments(arguments: Bundle?) {
+        if (arguments == null) {
+            return
+        }
+
+        // 1
+        currentPage.apply {
+            addSource(renderer) { renderer ->
+                GlobalScope.launch {
+                    val document = document.value
+
+                    if (document != null) {
+                        val bookmarks = interactors.getBookmarks(document).lastOrNull()?.page ?: 0
+                        postValue(renderer.openPage(bookmarks))
+                    }
+                }
+            }
+        }
+
+// 2
+        val documentFromArguments = arguments.get(DOCUMENT_ARG) as Document? ?: Document.EMPTY
+
+// 3
+        val lastOpenDocument = interactors.getOpenDocument()
+
+// 4
+        document.value = when {
+            documentFromArguments != Document.EMPTY -> documentFromArguments
+            documentFromArguments == Document.EMPTY && lastOpenDocument != Document.EMPTY -> lastOpenDocument
+            else -> Document.EMPTY
+        }
+
+// 5
+        document.value?.let { interactors.setOpenDocument(it) }
+
+    }
+
+    fun openDocument(uri: Uri) {
+        // TODO open document
+    }
+
+    fun openBookmark(bookmark: Bookmark) {
+        openPage(bookmark.page)
+    }
+
+    private fun openPage(page: Int) = renderer.value?.let {
+        currentPage.value = it.openPage(page)
+    }
+
+    fun nextPage() = currentPage.value?.let { openPage(it.index.plus(1)) }
+
+    fun previousPage() = currentPage.value?.let { openPage(it.index.minus(1)) }
+
+    fun reopenPage() = openPage(currentPage.value?.index ?: 0)
+
+    fun toggleBookmark() {
+        // TODO toggle bookmark on the current page
+    }
+
+    fun toggleInLibrary() {
+        // TODO toggle if open document is in library
+    }
 }
